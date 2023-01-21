f3cb7acb5b92debc67900ef210ee84627a5bba35
pon-pn <75652042+pon-pn@users.noreply.github.com>
2023-01-21 18:23:48 +0900

    スタイル調整

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,11 +2,11 @@ import Head from "next/head";
 import Link from "next/link";
 import { type ReactNode } from "react";
 
-const siteTitle = "My blog";
+const siteTitle = "日々のログ";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-800 dark:text-gray-100">
+    <div className="min-h-screen bg-white dark:bg-slate-900 text-slate-500 dark:text-slate-400">
       <Head>
         <title>{siteTitle}</title>
       </Head>
@@ -21,10 +21,14 @@ export default function Layout({ children }: { children: ReactNode }) {
           </p>
         </nav>
       </header>
-      <main className="container mx-auto max-w-2xl px-8 py-12 shadow-md bg-white dark:bg-gray-900">
-        {children}
+      <main className="p-8">
+        <div className="grid grid-cols-5 gap-5">
+          <li className="text-sky-500 dark:text-sky-400 cursor-pointer block rounded-lg shadow-md hover:bg-gray-100 shadow-lg max-w-2xl px-8 py-4 bg-white">
+            <p>{children}</p>
+          </li>
+        </div>
       </main>
-      <footer className="container mx-auto max-w-2xl px-8 py-12 text-sm">
+      <footer className="container mx-auto max-w-2xl p-4 text-sm">
         <nav>
           <ul className="flex flex-row gap-6 justify-center">
             <li>
